8994db1973e52b16db97e421996d87aeace5af1d
RenatoAO <renato.a.o@outlook.com>
2025-11-30 15:23:25 -0300

    Alterado script do player e camera

---
+++ Assets/@Project/Scripts/Background.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+
+public class Background : MonoBehaviour
+{
+ 
+    public Vector2 m_speed;
+
+    
+    private void Update()
+    {
+        
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+
+        
+        var material = spriteRenderer.material;
+
+        
+        material.mainTextureOffset += m_speed * Time.deltaTime;
+    }
+}
